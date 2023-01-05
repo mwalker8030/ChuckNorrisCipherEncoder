2424f104ef430472a3a63bc010ab3a9ab477e428
bansh <ban8029@protonmail.com>
2023-01-04 17:56:58 -0800

    made the bits concatenate on one string to read and encode everything on one line.

--- Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
+++ Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
@@ -1,29 +1,29 @@
 package chucknorris;
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.lang.Math;
 
 public class Main {
 
-    public static void encode(char a){
+    public static void encode(char a) {
         String d = Integer.toBinaryString(a);
         String dd = String.format("%07d\n", Integer.parseInt(d));
         int len = dd.length(), s = 1;
         char c = dd.charAt(0);
-        for(int i = 1; i<len;i++){
-            if(c == dd.charAt(i)){
+        for (int i = 1; i < len; i++) {
+            if (c == dd.charAt(i)) {
                 ++s;
-                continue;
-            }else{
-                if(c == '1'){
+            } else {
+                if (c == '1') {
                     System.out.print("0 ");
-                }else{
+                } else {
                     System.out.print("00 ");
                 }
-                while(s>0){System.out.print("0");--s;}
-                if(i+1!=len)
+                while (s > 0) {
+                    System.out.print("0");
+                    --s;
+                }
+                if (i + 1 != len)
                     System.out.print(" ");
-                c=dd.charAt(i);
+                c = dd.charAt(i);
                 s++;
             }
         }
@@ -31,69 +31,50 @@ public class Main {
     }
 
     static int s = 1;
-    public static void connectMatch(char c, String a, int i){
-        if(c == a.charAt(i)){
-            ++s;
-            return;
-        }else{
-            while(s>0){System.out.print("0");--s;}
-            if(i+1 != a.length())
-                System.out.print(" ");
-            c = a.charAt(i);
-            ++s;
-        }
-    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Input string:");
         String u = scan.nextLine();
         int len = u.length();
         System.out.println("\nThe result:");
-        ArrayList<String> arr = new ArrayList<>();
-        for(int i=0; i < len; i++){
+        //ArrayList<String> arr = new ArrayList<>();
+        StringBuilder con = new StringBuilder();
+        for (int i = 0; i < len; i++) {
             String d = Integer.toBinaryString(u.charAt(i));
             System.out.printf("%c = %07d :", u.charAt(i), Integer.parseInt(d));
             encode(u.charAt(i));
-            arr.add(String.format("%07d\n", Integer.parseInt(d)));
+            con.append(String.format("%07d", Integer.parseInt(d)));
             System.out.println();
         }
 
-        boolean init;
-        char prev = '1';
-        for(String a: arr){
-            len = a.length();
-            s = 1;
-            char c = a.charAt(0);
-            init = false;
-
-            for(int i = 1; i<len;i++){
-                if(init && prev == a.charAt(0)){
-                    if(c == a.charAt(i)){
-                        connectMatch(c,a,i);
-                        prev = c;
-                        continue;
-                    }else{
-                        prev = c;
-                    }
-                }else{
-                    init = true;
-                }
-                if(c == a.charAt(i)){
-                    ++s;
+        System.out.println(con);
+        s = 1;
+        char c = con.charAt(0);
+        len = con.length();
+        for (int i = 1; i < len; i++) {
+            while (c == con.charAt(i)) {
+                ++s;
+                if (i + 1 < len) {
+                    c = con.charAt(i);
+                    ++i;
                     continue;
-                }else{
-                    if(c == '1'){
-                        System.out.print("0 ");
-                    }else{
-                        System.out.print("00 ");
-                    }
-                    while(s>0){System.out.print("0");--s;}
-                    if(i+1!=len)
-                        System.out.print(" ");
-                    c = a.charAt(i);
-                    s++;
                 }
+                break;
             }
+
+            if (c == '1') {
+                System.out.print("0 ");
+            } else {
+                System.out.print("00 ");
+            }
+            while (s > 0) {
+                System.out.print("0");
+                --s;
+            }
+            if (i + 1 != len)
+                System.out.print(" ");
         }
+
     }
 }
