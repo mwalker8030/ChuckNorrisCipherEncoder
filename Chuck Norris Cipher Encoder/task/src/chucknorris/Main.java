cf5e37a36d3fad7c06c05bb3f16729d4467b5d10
bansh <ban8029@protonmail.com>
2023-01-05 22:03:56 -0800

    Added a decoder for the chuck assignment. only decodes to 7-Bit ASCII.

--- Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
+++ Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
@@ -5,11 +5,13 @@ public class Main {
     public static class Chuck{
         StringBuilder code, enc;
         char c;
+        int i;
         public Chuck(){
             code = new StringBuilder();
-            enc = new StringBuilder("The result:\n");
-            System.out.println("Input string:");
+            enc = new StringBuilder();
+            System.out.println("Input string:\n\n\"The result:");
             c = ' ';
+            i = 0;
         }
 
     }
@@ -39,7 +41,8 @@ public class Main {
             chuckIt(obj, i);
         }
 
-        System.out.print(obj.enc);
+        System.out.println(obj.enc);
+        System.out.print(unChuckIt(obj));
     }
 
     private static void chuckIt(Chuck obj, int i) {
@@ -54,5 +57,41 @@ public class Main {
         s=1;
         if (i < obj.code.length())
             obj.enc.append(" ");
+    }
+
+    private static int findWhitespace(Chuck obj){
+        if(Character.isWhitespace(obj.enc.charAt(obj.i))){++obj.i;}
+        int counter = 0;
+        while(obj.i < obj.enc.length() && !Character.isWhitespace(obj.enc.charAt(obj.i))){
+            ++counter;
+            ++obj.i;
+        }
+        return counter;
+    }
+    private static String unChuckIt(Chuck obj){
+        System.out.println("decoding " + obj.enc + " to ");
+        String dec = "";
+        char c = ' ';
+        int counter = 0, len = obj.enc.length();
+        for (obj.i = 0; obj.i < len; obj.i++)
+        {
+            //find the value of the character
+            counter = findWhitespace(obj);
+
+            //hit a ws and identify symbol as one or zero
+            c = (counter == 2 ? '0' : '1');
+
+            //count zeros until another ws
+            counter = findWhitespace(obj);
+
+            //append to string the appropriate amount of symbols
+            for (;counter > 0; --counter){
+                dec += c;
+            }
+        }
+        //print decrypted value
+        System.out.println("decrypted code is : " + dec);
+        return dec;
+
     }
 }
--- Topics/Processing strings/The longest word/src/Main.java
+++ Topics/Processing strings/The longest word/src/Main.java
@@ -1,24 +1,27 @@
 import java.util.Scanner;
+import java.lang.Character;
 
 class Main {
     public static void main(String[] args) {
         // put your code here
         Scanner scan = new Scanner(System.in);
-        StringBuilder uinput = new StringBuilder(scan.nextLine());
+        StringBuilder userInput = new StringBuilder(scan.nextLine());
         StringBuilder word = new StringBuilder();
         StringBuilder longest = new StringBuilder();
         int i = 0;
-        while(i < uinput.length()){
-            if(uinput.charAt(i) != ' '){
-                word.append(uinput.charAt(i));
+        while (i < userInput.length()) {
+            if(!Character.isWhitespace(userInput.charAt(i))) {
+                word.append(userInput.charAt(i));
             }
 
-            if(word.length() > longest.length()){
+            if (word.length() > longest.length()) {
                 longest.setLength(0);
                 longest.append(word);
             }
 
-            if(uinput.charAt(i) == ' '){word.setLength(0);}
+            if (Character.isWhitespace(userInput.charAt(i))) {
+                word.setLength(0);
+            }
 
             i++;
         }
